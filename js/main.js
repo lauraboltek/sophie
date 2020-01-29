8cc5a20905fdbb3b5bd7ed64738c591e82784af6
Laura Boltek <boltek.l@gmail.com>
2020-01-29 15:56:10 +0100

    13. dan

--- js/main.js
+++ js/main.js
@@ -7,72 +7,81 @@ $(function() {
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		responsive: [
-		  {
+		{
 			breakpoint: 1024,
 			settings: {
-			  slidesToShow: 3,
-			  slidesToScroll: 3,
-			  infinite: true,
-			  dots: true
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			dots: true
 			}
-		  },
-		  {
+		},
+		{
 			breakpoint: 600,
 			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2
+			slidesToShow: 2,
+			slidesToScroll: 2
 			}
-		  },
-		  {
+		},
+		{
 			breakpoint: 480,
 			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
+			slidesToShow: 1,
+			slidesToScroll: 1
 			}
-		  }
-	
+		},
+		{
+			breakpoint: 430,
+			settings: "unslick"
+		}
 		]
-	  });
+	});
 
 	 
 	 $('.faq-item-title').on('click', function() {
 		 $(this).parent().toggleClass('active');
 	 });
+
+	 $('.fancybox-media').fancybox({
+		openEffect  : 'none',
+		closeEffect : 'none',
+		helpers : {
+			media : {}
+		}
+	});
+	
+	window.onscroll = function() {myFunction()};
+	
+	var header = document.getElementById("nav-sticky");
+	var sticky = header.offsetTop;
+	
+	function myFunction() {
+	  if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	  } else {
+		header.classList.remove("sticky");
+	  }
+	}
+	
+	$('.btn-toggle-nav').on('click', function() {
+		$('body').toggleClass('active-nav');
+	});
+	
+	
+	var scrollPosition = 0;
+	$(window).on('scroll', function() {
+		var currScrollPosition = $(this).scrollTop(),
+			toTop = $('a.to-top'),
+	
+		scrollPosition = currScrollPosition;
+	
+		if(scrollPosition > 400) {
+			toTop.addClass('active');
+		} else {
+			toTop.removeClass('active');
+		}
+	});	 
 })
 
 
-$('.fancybox-media').fancybox({
-	openEffect  : 'none',
-	closeEffect : 'none',
-	helpers : {
-		media : {}
-	}
-});
 
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("nav-sticky");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-
-var scrollPosition = 0;
-$(window).on('scroll', function() {
-	var currScrollPosition = $(this).scrollTop(),
-		toTop = $('a.to-top'),
-
-	scrollPosition = currScrollPosition;
-
-	if(scrollPosition > 400) {
-		toTop.addClass('active');
-	} else {
-		toTop.removeClass('active');
-	}
-});

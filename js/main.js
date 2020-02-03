4267ca10b5fb064ba718b6b7ef2e66a301958cdf
Mile Lazar <mile@marker.hr>
2020-02-03 10:46:43 +0100

    observer

--- js/main.js
+++ js/main.js
@@ -39,34 +39,21 @@ $(function() {
 
 
 	//bubble
-	const sections = document.querySelectorAll('section');
-	const bubble = document.querySelector('bubble');
-	const bcgcolor = ['#544a49'];
-
+	const sections = document.querySelectorAll('.section');
 	const options = {
 		threshold: 0.7
 	};
-
 	let observer = new IntersectionObserver(navCheck, options);
 
 	function navCheck(entries){
 		entries.forEach(entry => {
-			const className = entry.target.className;
-			const activeAnchor = document.querySelector(`[data-page=${className}]`);
-			const bcgcolor = entry.target.getAttribute('data-index');
-			const coords = activeAnchor.getBoundingClientRect();
-			const directions = {
-				height: coords.height,
-				width: coords.width,
-				top: coords.top,
-				left: coords.left
-			};
 			if(entry.isIntersecting){
-				bubble.style.setProperty('left', `&{directions.left}px`);
-				bubble.style.setProperty('top', `&{directions.top}px`);
-				bubble.style.setProperty('width', `&{directions.width}px`);
-				bubble.style.setProperty('height', `&{directions.height}px`);
-				bubble.style.background = bcgcolor[bcgcolor];
+				let index = entry.target.getAttribute('data-index');
+				if(index) {
+					console.log(index);
+					$('.nav a').removeClass('active');
+					$('.nav a[data-index='+index+']').addClass('active');
+				}
 			}
 		})
 	}
